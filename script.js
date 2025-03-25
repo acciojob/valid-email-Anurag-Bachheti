e278367a2f0c4cc9600c277b39f893b5c5b71774
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-25 16:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function validEmail(str) {
-  //your JS code here.
-}
+   let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|in)$/;
+	if(emailPattern.test(str)){
+		return true;
+	}else{
+	return false;
+	}
+}	
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
